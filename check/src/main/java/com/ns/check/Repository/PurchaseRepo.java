103a4a10cca81f519a38a210aeb269487687da52
vmuralidharan <vmuralidharan@nextsphere.com>
2019-08-12 14:50:34 +0530

    deleted the duplicated files

--- check/src/main/java/com/ns/check/Repository/PurchaseRepo.java
+++
@@ -1,7 +0,0 @@
-package com.ns.check.Repository;
-
-import com.ns.check.Model.Purchase;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PurchaseRepo extends JpaRepository<Purchase,Long> {
-}
--- check/src/main/java/com/ns/check/Repository/ResultantModelRepo.java
+++
@@ -1,7 +0,0 @@
-package com.ns.check.Repository;
-
-import com.ns.check.Model.ResultantModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ResultantModelRepo extends JpaRepository<ResultantModel,Long> {
-}
